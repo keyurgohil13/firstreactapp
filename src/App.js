8c4373659f94823701e8cd6eac0eae80f821b337
keyurgohil13 <gohilkeyur13@gmail.com>
2022-11-18 14:54:05 +0530

    second day

--- src/App.js
+++ src/App.js
@@ -19,11 +19,19 @@ function App() {
         </a>
       </header> */}
 
-      <h1>
+      {/* <h1>
         Hello World
       </h1>
       <p>
         lorem 
+      </p> */}
+
+      <h2>
+        Day2
+      </h2>
+
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dignissim quis mauris ac fermentum. Donec non feugiat sapien. Sed purus mauris, euismod sed vestibulum vitae, feugiat et lacus. Aliquam iaculis arcu id nulla rhoncus pretium. Integer facilisis pellentesque magna, eget ultricies nulla elementum at. Vivamus cursus pellentesque orci, sit amet mollis sapien rhoncus sit amet. Sed malesuada nisl ut urna auctor, non interdum augue consectetur. Phasellus porta dolor sit amet massa dignissim, in congue leo placerat. Donec non libero nec mi lacinia vestibulum. Quisque ullamcorper, tortor vel vulputate fermentum, justo ipsum placerat nunc, quis volutpat dolor augue in ligula. Praesent vel ornare nisi. Phasellus sed ex et sem tincidunt ornare. Nunc auctor sem in semper vulputate. Nulla sit amet ante et sapien mattis fringilla. Vivamus eget vehicula nisl. Maecenas in tortor metus.
       </p>
     </div>
   );
